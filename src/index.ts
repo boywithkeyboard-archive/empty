3e0a845374ecefaaa95af98cfbc5234f9ecb4d68
Azury <azury.wtf@gmail.com>
2022-08-02 15:29:33 +0200

    chore: add code

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { unlink } from 'fs/promises'
+import { join } from 'path'
+import files from '@unvented/files'
+import minimist from 'minimist'
+
+(async () => {
+  const argv = minimist(process.argv.slice(2))
+
+  for (let directory of argv._) {
+    directory = join(process.cwd(), directory.startsWith('./') ? directory : `./${directory}`)
+
+    for await (const file of files(directory))
+      await unlink(file)
+  }
+})()
